b28ccc9d42cd89ca4ca3ea9e48e75e16fba20a9c
Denis Malyshev <mdvlift@gmail.com>
2020-06-13 13:47:52 +0400

    Task 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,44 @@
+"use strict"
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x = [];
+    let d = Math.pow(b,2) - 4 * a * c;
+    if (d < 0) {
+        return x;
+    } else if (d === 0 ) {
+        x.push(-b / (2 * a));
+    } else if (d > 0) {
+        x.push((-b + Math.sqrt(d)) / (2 * a));
+        x.push((-b - Math.sqrt(d)) / (2 * a));
+        Math.sqrt(13);
+    }
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
-}
+    let summ = 0;
+    if (marks.length === 0 ) {
+        return 0;
+    } else if (marks.length > 5){
+        console.log("Оценок больше 5");
+        marks.splice(5);
+    }
+    for (let point of marks) {
+        summ += point;  
+    }
+    return summ / marks.length;
+} 
+
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let dateUser = dateOfBirthday.getFullYear();
+    let data = new Date();
+    let currentYear = data.getFullYear();
+    let age = currentYear - dateUser;
+
+    if (age > 18) {
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
 }
