975d9538f7b7f9b38a47e7644f75d8c5904d6815
Denis Malyshev <drevnii94@mail.ru>
2020-06-18 16:22:50 +0400

    Task 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,26 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let arr = [percent, contribution, amount];
+    let param = ['процент', 'начальный взнос', 'общая стоимость'];
+    for (let i = 0; i < arr.length; i++){
+        let val = arr[i];
+        let t = ``;
+        if (isNaN(val)) {
+            return `Параметр ${param[i]} содержит неправильное значение ${val} \n`;
+        }    
+    }
+    let summRefund = amount - contribution;
+    let numberMonths = Math.round((date - new Date())/ 1000 / 60 / 60 / 24 /30);
+    let procent = (percent/100) / 12;
+    let montsPayment = summRefund * (procent + procent / (Math.pow((1 + procent),numberMonths)-1));
+    return ((montsPayment * numberMonths).toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name) {
+        return(`Привет, мир! Меня зовут ${name}`);
+    } else {
+        return(`Привет, мир! Меня зовут Аноним`);
+    }
 }
