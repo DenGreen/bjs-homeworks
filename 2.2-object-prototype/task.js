daaf2612bd87ec5b636cf980e828793c11f27d2f
Denis Malyshev <drevnii94@mail.ru>
2020-06-23 21:29:04 +0400

    Task2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,37 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+  let str = this.replace(/[^А-Я0-9]/gi, "").toLowerCase();
+  let removeStr = str.split("").reverse().join("");
+  if (str === removeStr) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let i = 0;
+  let average = 0;
+  if (marks.length > 0) {
+    for (let mark of marks) {
+      i++;
+      average += mark;
+    }
+    let roundedAverage = Math.round(average / i);
+    return roundedAverage;
+  } else {
+      return 0;
+  }
+  
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = new Date();
+    birthday = new Date(birthday);
+    let diff = now.getTime() - birthday.getTime();
+    let age = diff / 31536000000;
+    if (age > 18) {
+        return true;
+    } else {
+        return false;
+    }
 }
